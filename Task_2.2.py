7bda4d098e0cd5e911ee3441f00c482e0068a000
KatherineLinchevskaya <preludeh908@gmail.com>
2023-02-21 09:38:01 +0200

    Task_2.2 done

---
+++ Task_2.2.py
@@ -0,0 +1,8 @@
+# Create variables john_age and marta_age of integer type.
+# Assign the created variables the values ​​you want. Print the values ​​of both variables to the console using the print method.
+
+john_age = int(25)
+marta_age = int(20)
+
+print("John's age:", john_age)
+print("Marta's age:", marta_age)
