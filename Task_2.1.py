72782a4c68e67ba0d6741b1e5c1a8485ff421487
KatherineLinchevskaya <preludeh908@gmail.com>
2023-02-21 09:35:00 +0200

    Task_2.1 done

---
+++ Task_2.1.py
@@ -0,0 +1,8 @@
+# Create variables john_salary and marta_salary of some type (floating point).
+# Assign the created variables the values ​​you want. Print the values ​​of both variables to the console using the print method.
+
+john_salary = float(20000.5)
+marta_salary = float(16000)
+
+print("John's salary:", john_salary)
+print("Marta's salary:", marta_salary)
