2eb350293db35ad38f1a17d92568f6568edbb9d9
KatherineLinchevskaya <preludeh908@gmail.com>
2023-02-21 09:49:44 +0200

    Task_2.4 done

---
+++ Task_2.4.py
@@ -0,0 +1,8 @@
+# Create boolean variables john_gender and marta_gender.
+# Let john be false and Marta be true. Print the values of both variables to the console using the print method.
+
+john_gender = False
+marta_gender = True
+
+print("John's gender:", john_gender)
+print("Marta's gender:", marta_gender)
