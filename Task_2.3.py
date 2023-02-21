a6d0d8c09de68d34f52882946a17dc26e6d40264
KatherineLinchevskaya <preludeh908@gmail.com>
2023-02-21 09:43:07 +0200

    Task_2.3 done

---
+++ Task_2.3.py
@@ -0,0 +1,8 @@
+# Create string type variables john_name and marta_name
+# Assign the created variables the values you want. Print the values of both variables to the console using the print method.
+
+boy_name = 'John'
+girl_name = 'Marta'
+
+print("boy's name:", boy_name)
+print("girl's name:", girl_name)
